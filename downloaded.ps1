33b88c5d7f712c87baf7e3bd23e3ac7029a938a8
jdhitsolutions <jhicks@jdhitsolutions.com>
2020-08-04 09:48:44 -0400

    updated downloaded.ps1

--- downloaded.ps1
+++ downloaded.ps1
@@ -1,5 +1,7 @@
 #this is a sample script to demonstrate what happens when you download a script in PowerShell
 
 Write-Host "This script doesn't do anything useful and isn't even well-written." -foregroundcolor yellow
+
 $m = Get-Random -minimum 1 -maximum 100
-write-Host "Your magic number is $m" -foreground green
+
+Write-Host "Your magic number is $m" -foregroundcolor green
